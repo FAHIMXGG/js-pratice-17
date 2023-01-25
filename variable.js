fc8d744805ed1b41930456e6c4aef779d08ed4d6
FAHIMXGG <ahasanulhaquefahim1@gmail.com>
2023-01-26 01:26:09 +0600

    1st

--- variable.js
+++ variable.js
@@ -5,15 +5,44 @@ var price = 40;
 var age = 20;
 var temperature = 32;
 var people = 10;
-var person = fahim;
+
+var apple = 11;
+var banana = 41;
+var lichi = 51;
+var mango = 16;
+var pineapple = 17;
+var total = apple + banana;
 
 // string
 
 var location = "dhaka BD";
 var color = "orange green"
+var person = "fahim";
 
 //Boolean
 
 var serious = true;
 var isFullMarks = true;
 var isSingle = false;
+
+console.log(location)
+console.log(price)
+console.log(people)
+console.log(age)
+console.log(person)
+console.log(serious)
+console.log(people/age)
+console.log(people*age)
+console.log(total)
+console.log(apple+mango+lichi+pineapple)
+
+
+var difference = apple-lichi;
+console.log(difference);
+//
+
+var orangePrice = 56;
+var quantity = 7;
+var orangeCost = (orangePrice*quantity)
+
+console.log(orangeCost); 
