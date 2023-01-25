ad9b8613fdb440b5252341e3a68191de2af544eb
FAHIMXGG <ahasanulhaquefahim1@gmail.com>
2023-01-26 00:27:22 +0600

    first commit

---
+++ variable.js
@@ -0,0 +1,19 @@
+// var memory 
+// numeric
+
+var price = 40;
+var age = 20;
+var temperature = 32;
+var people = 10;
+var person = fahim;
+
+// string
+
+var location = "dhaka BD";
+var color = "orange green"
+
+//Boolean
+
+var serious = true;
+var isFullMarks = true;
+var isSingle = false;
