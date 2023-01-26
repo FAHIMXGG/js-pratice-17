0e1a234bfa15055bf6ed72e56d8733dd701aac03
FAHIMXGG <ahasanulhaquefahim1@gmail.com>
2023-01-26 17:14:01 +0600

    st3

---
+++ conversion-2.js
@@ -0,0 +1,18 @@
+var hello = 44;
+var bye = 33;
+var total = hello+bye;
+console.log(total)
+
+var hellos = "64";
+var byes = "32";
+var totals = hellos+byes;
+
+//parseInt (convert numb) porno number only
+//parseFloat any numb
+var convert = parseInt(hellos);
+var convert2 = parseInt(byes);
+console.log(convert);
+
+var covertTotal = convert+convert2;
+console.log(covertTotal);
+
---
+++ conversion.js
@@ -0,0 +1,4 @@
+var firstName = "jennie";
+var lastName = "Kim";
+var fullName = firstName+" "+lastName;
+console.log(fullName);
