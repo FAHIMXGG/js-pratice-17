32ec3359d7f12bdbe748a856e931ef6aee241936
FAHIMXGG <ahasanulhaquefahim1@gmail.com>
2023-01-26 17:48:58 +0600

    3rd

---
+++ type.js
@@ -0,0 +1,16 @@
+// typeof number/String/boolean
+var price = 100;
+console.log(typeof price);
+
+// not define
+var romantic;
+console.log(typeof romantic);
+
+// Tofixed 0.***(3)
+var numb1 = 0.1;
+var numb2 = 0.2;
+var sum = numb1+numb2;
+sum = sum.toFixed(3);
+sum = parseFloat(sum);
+console.log(sum);
+
