f7a6ca5acefc385422d1634091c0c40fbbd8189e
FAHIMXGG <ahasanulhaquefahim1@gmail.com>
2023-01-26 16:50:00 +0600

    s2

---
+++ varrible-name.js
@@ -0,0 +1,21 @@
+// var change (update)
+var people = 66;
+people = 1010;
+console.log(people);
+
+//existing value
+var apple = 22;
+apple = apple + 44;
+console.log(apple)
+
+// addition to the same variable shorthand
+// += means adding some value to the aexisting value
+var lemon = 10;
+lemon += 30;
+console.log(lemon)
+
+// ++/-- means  add -+1
+var age = 50;
+age++;
+console.log(age);
+
